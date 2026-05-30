4397b590b1991ea5744885cd2144ea4e04235fe7
mrvauxs <mrvauxs@gmail.com>
2026-05-30 16:45:48 +0200

    add isValidUpdater util

--- templates/vite/src/lib/utils.ts
+++ templates/vite/src/lib/utils.ts
@@ -1,1 +1,11 @@
+import Document from "@7h3laughingman/foundry-types/common/abstract/document.mjs";
+
 export const dev = import.meta.env.DEV;
+
+export function isValidUpdater(data: Document, update?: Record<string, unknown>): boolean {
+	const isThereAnActiveGM = game.users.activeGM;
+	// No GM, see if you can do it yourself.
+	if (!isThereAnActiveGM) return data.canUserModify(game.user, "update", update);
+	// If there is a GM, you have to be the GM to update.
+	return game.users.activeGM?.isSelf || false;
+}
